f78f4dbd23c082e91d49c67ba6c454130228e1e1
Lizaveta <lizadondysh@icloud.com>
2024-02-09 22:33:16 +0200

    feat: add tests for 02

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,85 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+import { TIME_LIMIT } from 'utils/constants';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  {
+    message: 'should add two numbers',
+    a: 2,
+    b: 6,
+    action: Action.Add,
+    expected: 8,
+  },
+  {
+    message: 'should subtract two numbers',
+    a: 12,
+    b: 8,
+    action: Action.Subtract,
+    expected: 4,
+  },
+  {
+    message: 'should multiply two numbers',
+    a: 7,
+    b: 6,
+    action: Action.Multiply,
+    expected: 42,
+  },
+  {
+    message: 'should divide two numbers',
+    a: 24,
+    b: 4,
+    action: Action.Divide,
+    expected: 6,
+  },
+  {
+    message: 'should exponentiate two numbers',
+    a: 2,
+    b: 8,
+    action: Action.Exponentiate,
+    expected: 256,
+  },
+  {
+    message: 'should return null for invalid action',
+    a: 4,
+    b: 16,
+    action: 'Drink coffee',
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid arguments',
+    a: 2,
+    b: '8',
+    action: Action.Multiply,
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid arguments',
+    a: 2,
+    b: BigInt(8),
+    action: Action.Multiply,
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid arguments',
+    a: 2,
+    b: [8],
+    action: Action.Multiply,
+    expected: null,
+  },
+  {
+    message: 'should return null for invalid arguments',
+    a: 2,
+    b: { b: 8 },
+    action: Action.Multiply,
+    expected: null,
+  },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    `$message`,
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+    TIME_LIMIT,
+  );
 });
